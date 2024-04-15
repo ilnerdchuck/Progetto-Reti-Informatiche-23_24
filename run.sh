247aed7c3618958d37825990fe98f391ff3ba6ab
ilnerdchuck <fmignone98@gmail.com>
2024-04-15 23:06:54 +0200

    add: run script

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ make clean
 make build
 read -p "Compilazione eseguita. Premi invio per eseguire..."
 
-gnome-terminal -x sh -c "./build/server; exec bash"
-gnome-terminal -x sh -c "./build/client; exec bash"
-gnome-terminal -x sh -c "./build/client; exec bash"
+gnome-terminal -x sh -c "./build/server; exec bash" -t "SERVER" -geometry 100x30 
+gnome-terminal -x sh -c "./build/client; exec bash" -t "CLIENT-1"
+gnome-terminal -x sh -c "./build/client; exec bash" -t "CLIENT-2"
 
 
 alacritty -x sh -c "./build/server; exec bash"
