8dcd3563a87d6b244d3136da0c716f822bfd2e1d
ilnerdchuck <fmignone98@gmail.com>
2024-04-15 23:13:22 +0200

    add: run script

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ make build
 read -p "Compilazione eseguita. Premi invio per eseguire..."
 
 gnome-terminal --geometry=80x30 -x sh -c "setterm -term linux -back blue -fore white; ./build/server; exec bash" -t "SERVER" 
-gnome-terminal -x sh -c "./build/client; exec bash" -t "CLIENT-1"
-gnome-terminal -x sh -c "./build/client; exec bash" -t "CLIENT-2"
+gnome-terminal -x sh -c "setterm -term linux -back red -fore white; ./build/client; exec bash" -t "CLIENT-1"
+gnome-terminal -x sh -c "setterm -term linux -back brown -fore white; ./build/client; exec bash" -t "CLIENT-2"
 
 
 alacritty -x sh -c "./build/server; exec bash"
